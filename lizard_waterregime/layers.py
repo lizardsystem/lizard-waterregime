caacb467dd81358817dcdb8e02f459c151298937
arjanverkerk <arjan.verkerk@nelen-schuurmans.nl>
2011-03-16 14:59:17 +0000

    added adapter stub

---
+++ lizard_waterregime/layers.py
@@ -0,0 +1,10 @@
+from lizard_map.workspace import WorkspaceItemAdapter
+
+class AdapterWaterbalance(WorkspaceItemAdapter):
+    """Adapter for module LizardWaterbalance.
+
+    Registered as adapter_waterbalance.
+
+    Uses default database table "waterbalance_shape" as geo database.
+    """
+
