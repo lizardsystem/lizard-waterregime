d901dbb83238922400a2322e92c58e8e5f0947db
byrman <carsten.byrman@nelen-schuurmans.nl>
2011-03-17 16:49:24 +0000

    Removed waterregime_viewer. Not needed?

--- lizard_waterregime/urls.py
+++ lizard_waterregime/urls.py
@@ -5,9 +5,8 @@ from django.contrib import admin
 
 admin.autodiscover()
 
-urlpatterns = patterns('lizard_waterregime.views',
-    url(r'^$', 'start', name='waterregime_start'),
-    url(r'^viewer/$', 'waterregime_viewer', name="waterregime_viewer"),
+urlpatterns = patterns('',
+    url(r'^$', 'start', name='lizard_waterregime.views.waterregime_start'),
 )
 
 if settings.DEBUG:
