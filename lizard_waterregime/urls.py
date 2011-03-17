336ae7908e06723a447d78e769f19e2b19714b4e
byrman <carsten.byrman@nelen-schuurmans.nl>
2011-03-17 15:47:46 +0000

    Added url for waterregime_viewer.

--- lizard_waterregime/urls.py
+++ lizard_waterregime/urls.py
@@ -5,9 +5,9 @@ from django.contrib import admin
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # waterregime urls
-    url(r'^$', 'lizard_waterregime.views.start', name='waterregime_start'),
+urlpatterns = patterns('lizard_waterregime.views',
+    url(r'^$', 'start', name='waterregime_start'),
+    url(r'^viewer/$', 'waterregime_viewer', name="waterregime_viewer"),
 )
 
 if settings.DEBUG:
