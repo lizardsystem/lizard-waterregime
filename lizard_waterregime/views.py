6da4320968317cdf493b02df23aff6d11c611029
byrman <carsten.byrman@nelen-schuurmans.nl>
2011-03-17 14:24:48 +0000

    Added title to crumbs dict. Commented out unused imports.

--- lizard_waterregime/views.py
+++ lizard_waterregime/views.py
@@ -2,34 +2,35 @@
 
 # Create your views here.
 
-import datetime
-import logging
-import time
+#import datetime
+#import logging
+#import time
 
 from django.core.urlresolvers import reverse
-from django.core.cache import cache
-from django.http import HttpResponse
-from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404
+#from django.core.cache import cache
+#from django.http import HttpResponse
+#from django.http import HttpResponseRedirect
+#from django.shortcuts import get_object_or_404
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from django.utils import simplejson
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from matplotlib.lines import Line2D
-import mapnik
-import pkg_resources
+#from django.utils import simplejson
+#from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+#from matplotlib.lines import Line2D
+#import mapnik
+#import pkg_resources
 
-from lizard_map import coordinates
-from lizard_map.adapter import Graph
-from lizard_map.daterange import current_start_end_dates
-from lizard_map.daterange import DateRangeForm
-from lizard_map.models import Workspace
-from timeseries.timeseriesstub import TimeseriesStub
-from timeseries.timeseriesstub import grouped_event_values
-from timeseries.timeseriesstub import multiply_timeseries
+#from lizard_map import coordinates
+#from lizard_map.adapter import Graph
+#from lizard_map.daterange import current_start_end_dates
+#from lizard_map.daterange import DateRangeForm
+#from lizard_map.models import Workspace
+#from timeseries.timeseriesstub import TimeseriesStub
+#from timeseries.timeseriesstub import grouped_event_values
+#from timeseries.timeseriesstub import multiply_timeseries
 
-import hotshot
-import os
+#import hotshot
+#import os
+
 
 def start(request,
           template='lizard_waterregime/lizard_waterregime.html',
@@ -44,7 +45,7 @@ def start(request,
     """
 
     if crumbs_prepend is None:
-        crumbs = [{'name': 'home', 'url': '/'}]
+        crumbs = [{'name': 'home', 'title': 'hoofdpagina', 'url': '/'}]
     else:
         crumbs = list(crumbs_prepend)
 
