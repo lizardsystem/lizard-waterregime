6e6472be7d3a5700a4b2546c1e3f4ef048ad2708
arjanverkerk <arjan.verkerk@nelen-schuurmans.nl>
2011-03-17 09:01:50 +0000

    added stubs

--- lizard_waterregime/views.py
+++ lizard_waterregime/views.py
@@ -1,3 +1,66 @@
 # (c) Nelen & Schuurmans.  GPL licensed, see LICENSE.txt.
 
 # Create your views here.
+
+import datetime
+import logging
+import time
+
+from django.core.urlresolvers import reverse
+from django.core.cache import cache
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.utils import simplejson
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.lines import Line2D
+import mapnik
+import pkg_resources
+
+from lizard_map import coordinates
+from lizard_map.adapter import Graph
+from lizard_map.daterange import current_start_end_dates
+from lizard_map.daterange import DateRangeForm
+from lizard_map.models import Workspace
+from timeseries.timeseriesstub import TimeseriesStub
+from timeseries.timeseriesstub import grouped_event_values
+from timeseries.timeseriesstub import multiply_timeseries
+
+import hotshot
+import os
+
+def start(request,
+          template='lizard_waterregime/waterregime_start.html',
+          crumbs_prepend=None):
+    """Show waterbalance overview workspace.
+
+    The workspace for the waterbalance homepage should already be present.
+
+    Parameters:
+    * crumbs_prepend -- list of breadcrumbs
+
+    """
+    waterbalance_areas = WaterbalanceArea.objects.all()
+
+    if crumbs_prepend is None:
+        crumbs = [{'name': 'home', 'url': '/'}]
+    else:
+        crumbs = list(crumbs_prepend)
+
+    crumbs.append({'name': 'Waterregime',
+                   'title': 'Water Regime',
+                   'url': reverse('/waterregime/')})
+
+    special_homepage_workspace = \
+        get_object_or_404(Workspace, pk=WATERBALANCE_HOMEPAGE_KEY)
+    return render_to_response(
+        template,
+        {'waterbalance_areas': waterbalance_areas,
+         'workspaces': {'user': [special_homepage_workspace]},
+         'javascript_hover_handler': 'popup_hover_handler',
+         'javascript_click_handler': 'waterbalance_area_click_handler',
+         'crumbs': crumbs},
+        context_instance=RequestContext(request))
+
