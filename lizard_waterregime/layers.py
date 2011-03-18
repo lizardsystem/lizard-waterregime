0bdc9aba40faaf9f6118d36b6645e1d481cee85d
byrman <carsten.byrman@nelen-schuurmans.nl>
2011-03-18 16:49:25 +0000

    Added code for displaying layers.

--- lizard_waterregime/layers.py
+++ lizard_waterregime/layers.py
@@ -1,3 +1,6 @@
+import mapnik
+from django.conf import settings
+from lizard_map.coordinates import RD
 from lizard_map.workspace import WorkspaceItemAdapter
 
 
@@ -8,3 +11,67 @@ class AdapterWaterRegime(WorkspaceItemAdapter):
 
     Uses default database table "water_regime_shape" as geo database.
     """
+
+    def _mapnik_style(self):
+        """
+        Temp function to return a default mapnik style
+        """
+
+        def mapnik_rule(r, g, b, mapnik_filter=None):
+            """
+            Makes mapnik rule for looks. For lines and polygons.
+
+            From lizard_map.models.
+            """
+            rule = mapnik.Rule()
+            if mapnik_filter is not None:
+                rule.filter = mapnik.Filter(mapnik_filter)
+            mapnik_color = mapnik.Color(r, g, b)
+
+            symb_line = mapnik.LineSymbolizer(mapnik_color, 0)
+            rule.symbols.append(symb_line)
+
+            symb_poly = mapnik.PolygonSymbolizer(mapnik_color)
+            symb_poly.fill_opacity = 1
+            rule.symbols.append(symb_poly)
+            return rule
+
+        mapnik_style = mapnik.Style()
+        rule = mapnik_rule(255, 255, 0)
+        mapnik_style.rules.append(rule)
+        # for gid in range(100):
+        #     rule = mapnik_rule(0, 10 * gid ,0, '[gid] = %d' % gid)
+        #     mapnik_style.rules.append(rule)
+        rule = mapnik_rule(0, 255, 0, '[value] <= 75')
+        mapnik_style.rules.append(rule)
+        rule = mapnik_rule(255, 0, 0, '[value] > 75')
+        mapnik_style.rules.append(rule)
+        return mapnik_style
+
+    def layer(self, layer_ids=None, request=None):
+        """Generates and returns mapnik layers and styles.
+        """
+        
+        layers = []
+        styles = {}
+
+        db_settings = settings.DATABASES['default']
+        gid = self.layer_arguments["layer"]
+        table_view = '(select gid, the_geom, 100 as value from water_regime_shape where gid=%s) result_view' % gid
+
+        layer = mapnik.Layer('Geometry from PostGIS')
+        layer.srs = RD  #GOOGLE
+        layer.datasource = mapnik.PostGIS(
+            host=db_settings['HOST'],
+            user=db_settings['USER'],
+            password=db_settings['PASSWORD'],
+            dbname=db_settings['NAME'],
+            table=str(table_view))
+        layers.append(layer)
+
+        style_name = 'waterregime_style'
+        layer.styles.append(style_name)
+        mapnik_style = self._mapnik_style()
+        styles[style_name] = mapnik_style
+
+        return layers, styles
--- lizard_waterregime/views.py
+++ lizard_waterregime/views.py
@@ -10,7 +10,7 @@ from django.core.urlresolvers import reverse
 #from django.core.cache import cache
 #from django.http import HttpResponse
 #from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404
+#from django.shortcuts import get_object_or_404
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 #from django.utils import simplejson
@@ -21,9 +21,10 @@ from django.template import RequestContext
 
 #from lizard_map import coordinates
 #from lizard_map.adapter import Graph
-#from lizard_map.daterange import current_start_end_dates
-#from lizard_map.daterange import DateRangeForm
-from lizard_map.models import Workspace
+from lizard_map.daterange import current_start_end_dates
+from lizard_map.daterange import DateRangeForm
+#from lizard_map.models import Workspace
+from lizard_map.workspace import WorkspaceManager
 #from timeseries.timeseriesstub import TimeseriesStub
 #from timeseries.timeseriesstub import grouped_event_values
 #from timeseries.timeseriesstub import multiply_timeseries
@@ -31,13 +32,12 @@ from lizard_map.models import Workspace
 #import hotshot
 #import os
 
-def start(
-    request,
-    template='lizard_waterregime/lizard_waterregime.html',
-    special_homepage_workspace = get_object_or_404(
-        Workspace, pk=1
-    ),
-    crumbs_prepend=None):
+from lizard_waterregime.models import WaterRegimeShape
+
+
+def start(request,
+          template='lizard_waterregime/lizard_waterregime.html',
+          crumbs_prepend=None):
     """ Show waterregime homepage.
     """
 
@@ -50,13 +50,22 @@ def start(
                    'title': 'waterregime',
                    'url': reverse('waterregime_start')})
 
-    return render_to_response(
-        template,
+    workspace_manager = WorkspaceManager(request)
+    workspaces = workspace_manager.load_or_create()
+
+    date_range_form = DateRangeForm(
+        current_start_end_dates(request, for_form=True))
+
+    shapes = WaterRegimeShape.objects.all()
+
+    return render_to_response(template,
         {
-            'workspaces': {'user': [special_homepage_workspace]},
-            'javascript_hover_handler': 'popup_hover_handler',
+            'crumbs': crumbs,
+            'workspaces': workspaces,
+            'date_range_form': date_range_form,
+            'waterregime_shapes': shapes,
+#           'javascript_hover_handler': 'popup_hover_handler',
             'javascript_click_handler': 'waterbalance_area_click_handler',
             'use_workspaces': False,
-            'crumbs': crumbs
         },
         context_instance=RequestContext(request))
