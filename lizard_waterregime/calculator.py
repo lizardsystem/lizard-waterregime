80710976e056973a651d61c41e7a64eb6005444a
arjanverkerk <arjan.verkerk@nelen-schuurmans.nl>
2011-04-05 13:31:47 +0000

    Implemented new procedure

--- lizard_waterregime/calculator.py
+++ lizard_waterregime/calculator.py
@@ -9,15 +9,29 @@ from datetime import timedelta
 from numpy import abs
 from numpy import arange
 from numpy import array
+from numpy import concatenate
 from numpy import convolve
 from numpy import ones
 from numpy import vstack
+from numpy import zeros
+
+import logging
+logger = logging.getLogger('nens.calculator')
 
 def _first_of_hour(dt):
     """Return the first moment of the hour for a datetime."""
     return datetime(dt.year, dt.month, dt.day, dt.hour)
+    
+def _to_events(dates, values):
+    """Return an array of date,value pairs."""
+    return concatenate(([dates],[values])).transpose()
 
-# This *could* also be in the database. It relates timeseries
+def  _split_events(events):
+    """Return a (dates, values) tuple of arrays."""
+    return events[:,0],events[:,1]
+
+
+# This *could* also be in the database. It relates timeseries.
 timeseries_dict = {
         'E_HAZOF' : 'E_LELYSTAD',
         'E_LAZOF' : 'E_LELYSTAD',
@@ -40,7 +54,7 @@ class RegimeCalculator(object):
     """
     
     @classmethod
-    def map_events(p_series, e_series):
+    def map_events(cls, p_series, e_series):
         """
         """
 
@@ -50,179 +64,115 @@ class RegimeCalculator(object):
 
         for event in p_series:
             dt = event[0]
-            e = e_dict[datetime(dt.year, dt.month, dt.day)]
-            if not e:
+            try:
+                e = e_dict[datetime(dt.year, dt.month, dt.day)]
+            except KeyError:
                 e = cls.nearest(dt, e_series)
-            if e:
-                p_mapped.append(event)
-                e_mapped.append((dt, e))
+            p_mapped.append(event)
+            e_mapped.append((dt, e))
 
-        return p_mapped, e_mapped
-
-    @classmethod
-    def nearest(dt, series):
-        return None
+        return array(p_mapped), array(e_mapped)
 
     @classmethod
-    def weights(cls,r=1.5, tmax=7, stretch=1):
-        """Return an array of weightfactors with length (tmax * stretch).
+    def nearest(cls, dt, series):
+        return 0 #TODO
+
+    @classmethod
+    def weights(cls, r, tmax):
+        """Return an array of weightfactors"""
         
-        Each value represents the weight of a given value in the total P, E
-        or P-E. The most recent value, with the highest weight, is first,
-        since it is inded to be used with numpy.convolve() which flips its
-        second argument. The sum of the resulting list is equal to stretch.
-        In this way the result of the weighting is always a daily value, even
-        if the values to be weighted are for example hourly."""
         days = arange(-tmax + 1.,1)
         factors = 1 / abs(days - r)
-        stretched_factors = (factors.reshape(tmax, 1) *
-                                ones((tmax, stretch))).ravel()
-        normalized_factors = (stretched_factors / 
-                                sum(stretched_factors) * stretch)
-        return normalized_factors
-
+        normalized_factors = factors / sum(factors)
+        
+        a = concatenate((zeros(23),array([1]))).reshape(1,-1)
+        b = normalized_factors.reshape(-1,1)
+        
+        result = (a * b).ravel()
+        
+        return result
 
     @classmethod
-    def weighted_events(cls, events, weights):
-        """ Return array of weighted events."""
-        dates,values = array([list(events)]).transpose()
-
-        weighted_values = convolve(values.reshape(-1,),weights,'valid')
-        weighted_dates = dates[weights.size - 1:].reshape(-1)
-        return vstack((weighted_dates,weighted_values)).transpose()
+    def weighted_precipitation_surplus(
+        cls, shape, date1, date2):
+        """Return all data tuple of weighted events weighted. Since daily
+        values maybe required, everything is converted to daily."""
         
+        tmax = int(Constant.get('Tmax'))
+        r = abs(int(Constant.get('R_' + shape.afdeling)))
         
-    @classmethod
-    def make_hourly(cls, events):
-        """Return an array of hourly events.
+        extra_time = timedelta(days = tmax, hours = 23, seconds = -1)       
+        internal_start = date1 - extra_time
+
+        weights = cls.weights(r=r, tmax=tmax)
         
-        The events are expected to be daily at midnight. The values are
-        divided by 24. The extra dates come before the existing date."""
+        p_series = TimeSeriesFactory.get(timeseries_dict['P_' + shape.afdeling])
+        e_series = TimeSeriesFactory.get(timeseries_dict['E_' + shape.afdeling])
+            
+        p_events = tuple(p_series.events(internal_start, date2, missing=0))
+        eref_events = tuple(e_series.events(internal_start, date2, missing=0))
+        e_events = [(dt, v * shape.get_cropfactor(dt))
+                        for dt, v in eref_events]  
         
-        dates,values = array([list(events)]).transpose()
-
-        timedeltas = map(lambda h: timedelta(hours=h),arange(-23.,1.))
-        hourly_dates = (dates+timedeltas).flatten()
-
-        valuefactors = ones((1,24))/24.
-        hourly_values = (values*valuefactors).flatten()
+        p_aligned, e_aligned = cls.map_events(p_events, e_events)
         
-        return vstack((hourly_dates,hourly_values)).transpose()
+        p_daily_values = convolve(p_aligned[:,1],ones(24),'valid')
+        e_daily_values = e_aligned[23:,1]
+        dates =  p_aligned[23:,0]
 
-    @classmethod
-    def make_daily(cls, events):
-        """Return an array of daily events.
+        p_weighted = convolve(p_daily_values,weights,'valid')
+        e_weighted = convolve(e_daily_values,weights,'valid')
+        dates_weighted = dates[weights.size - 1:]
+
+        pmine_weighted = p_weighted - e_weighted
+
+        #print concatenate(([dates],[p_daily_values])).transpose()
         
-        The list of events is truncated to the last 24 * n values, n integer.
-        For every group of 24 events, the last date and the sum of the values
-        is returned."""
-        
-        events_list = list(events)
-        
-        # Make an array and truncate to last 24 * n values, n integer.
-        dates,values = array(events_list[len(events_list) % 24:]).transpose()
-        
-        dates.shape = (dates.size / 24, 24)
-        values.shape = (dates.size / 24, 24)
+        return (
+            _to_events(dates_weighted,pmine_weighted),
+            _to_events(dates,p_daily_values),
+            array(e_events),
+        )
 
-        daily_dates = map(max,dates)
-        daily_values = map(sum,values)
-
-        return vstack((daily_dates,daily_values)).transpose()
-
-    @classmethod
-    def subtract_event_values(cls, events_a, events_b):
-        """ Return events with dates from a and values a - b """
-        return [
-            (pair[0][0], pair[0][1] - pair[1][1])
-            for pair in zip(list(events_a),list(events_b))]
-
-
-    @classmethod
-    def multiply_event_values(cls, events_a, events_b):
-        """ Return events with dates from a and values a * b """
-        return [
-            (pair[0][0], pair[0][1] * pair[1][1])
-            for pair in zip(list(events_a),list(events_b))]
-
+    
     @classmethod
     def refresh(cls,dt):
         """ Check and if necessary insert precipitationsurplus values in
         database for datetime.
         
-        To be called from the waterregime adapter"""
-        
-        if PrecipitationSurplus.objects.filter(date=_first_of_hour(dt)).exists():
-            return False
-        else:
-            tmax = int(abs(Constant.get('Tmax')))
-            # 1 second less, to prevent a value to much
-            date1 = dt - timedelta(days=tmax, seconds=-1)
-            date2 = dt
+        To be called from the waterregime adapter."""
 
-            shapes = WaterRegimeShape.objects.all()
+        qs = PrecipitationSurplus.objects.filter(date=_first_of_hour(dt))       
+        # if PrecipitationSurplus.objects.filter(date=_first_of_hour(dt)).exists():
+        if bool(qs):
+            for obj in qs:
+                obj.delete()
+        # return False
 
-            for s in shapes:
-                pmine,p,e = cls.weighted_precipitation_surplus(
-                    s.afdeling, date1, date2)
-                p = PrecipitationSurplus(
-                    waterregimeshape=s,
-                    date=_first_of_hour(dt),
-                    value=pmine[0][1],
-                )
-                p.save()
 
-            return True
+        shapes = WaterRegimeShape.objects.all()
 
-    @classmethod
-    def weighted_precipitation_surplus(
-        cls, area, date1, date2):
-        """Return all data tuple of weighted events weighted. Since daily
-        values maybe required, everything is converted to daily."""
-        
-        tmax = abs(int(Constant.get('Tmax')))
-        r = abs(int(Constant.get('R_' + area)))
+        for s in shapes:
+            pmine,p,e = cls.weighted_precipitation_surplus(
+                s, dt, dt)
+            p = PrecipitationSurplus(
+                waterregimeshape=s,
+                date=_first_of_hour(dt),
+                value=pmine[0][1],
+            )
+            p.save()
 
-        weights = cls.weights(r=r, tmax=tmax, stretch=24)
-        shape = WaterRegimeShape.objects.get(afdeling=area)
-        
-        p_series = TimeSeriesFactory.get(timeseries_dict['P_' + area])
-        e_series = TimeSeriesFactory.get(timeseries_dict['E_' + area])
+        return True
 
-        
-        # P must be hourly anyway
-        if p_series.hours == 24:
-            p_events_hourly = cls.make_hourly(p_series.events(date1,date2))
-        elif p_series.hours == 1:
-            p_events_hourly = tuple(p_series.events(date1,date2))
-        # else: raise some error?
 
-        # E daily is also necessary, to reduce load on the 
-        # get_cropfactor() method.
-        if e_series.hours == 24:
-            eref_events_daily = tuple(e_series.events(date1,date2))
-            eref_events_hourly = cls.make_hourly(eref_events_daily)
-        elif e_series.hours == 1:
-            eref_events_hourly = tuple(e_series.events(date1,date2))
-            eref_events_daily = cls.make_daily(eref_events_hourly)
-        # else: raise some error?
-
-        c_events_daily = [(dt, shape.get_cropfactor(dt))
-                        for dt, v in eref_events_daily]
-        c_events_hourly = cls.make_hourly(c_events_daily)
-                        
-        e_events_hourly = cls.multiply_event_values(
-                            eref_events_hourly,c_events_hourly)
-
-                            
-        p_weighted = cls.weighted_events(p_events_hourly, weights)
-        e_weighted = cls.weighted_events(e_events_hourly, weights)
-        
-        pmine_weighted = cls.subtract_event_values(p_weighted, e_weighted)
-
-        return pmine_weighted,p_weighted,e_weighted
-    
     @classmethod
     def test(cls):
-        #cls.refresh(datetime.now())
-        RegimeCalculator.align((1,2), (1,2,3))
+        a,b,c = cls.weighted_precipitation_surplus(
+            WaterRegimeShape.objects.all()[0],
+            datetime.now(),
+            datetime.now()
+        )
+        print a
+        print b
+        print c
+
