9d41a941ee72ea2c8d57a8df54360ae0c27927c1
byrman <carsten.byrman@nelen-schuurmans.nl>
2011-04-07 11:08:43 +0000

    New implementation of map_events().

--- lizard_waterregime/calculator.py
+++ lizard_waterregime/calculator.py
@@ -11,6 +11,8 @@ from numpy import arange
 from numpy import array
 from numpy import concatenate
 from numpy import convolve
+from numpy import interp
+from numpy import NaN
 from numpy import ones
 from numpy import vstack
 from numpy import zeros
@@ -58,28 +60,44 @@ class RegimeCalculator(object):
         """
         """
 
-        p_mapped = []
         e_mapped = []
         e_dict = dict((event[0], event[1]) for event in e_series)
 
         for event in p_series:
-            
-            dt = event[0]
-            try:
-                e = e_dict[datetime(dt.year, dt.month, dt.day)]
-            except KeyError:
-                e = cls.nearest(dt, e_series)
-            # Carsten: note that bool(0) evaluates to false,
-            # days with zero e will drop out...
-            if e: 
-                p_mapped.append(event)
-                e_mapped.append((dt, e))
 
-        return array(p_mapped), array(e_mapped)
+            dt = event[0]
+
+            try:
+                dt_before = datetime(dt.year, dt.month, dt.day)
+                e_before = float(e_dict[dt_before])
+            except:
+                e_before = None
+
+            try:
+                dt_after = dt_before + timedelta(days=1)
+                e_after = float(e_dict[dt_after])
+            except:
+                e_after = None
+
+            if e_before is not None and e_after is not None:
+                delta = dt - dt_before
+                e_dt = interp(delta.seconds, (0, 86400), (e_before, e_after))
+            elif e_before is not None:
+                e_dt = e_before
+            elif e_after is not None:
+                e_dt = e_after
+            else:
+                e_dt = cls.nearest(dt, e_series)
+
+            e_mapped.append((dt, e_dt))
+
+        return array(p_series), array(e_mapped)
 
     @classmethod
     def nearest(cls, dt, series):
-        return None
+        """
+        """
+        return NaN
 
     @classmethod
     def weights(cls, r, tmax):
